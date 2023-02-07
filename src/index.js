9c8117b7dc6110b30cc889f684058fe6c518d80e
Nadinda <nadinda@nadinda.dev>
2023-02-07 20:02:21 +0100

    Replace Parcel 1 to Parcel 2 to use Vue 3

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import "./styles.css";
 import { trackPageview, trackEvent } from "./analytics-api.js";
 
 // Your code here
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+app.mount("#app");
